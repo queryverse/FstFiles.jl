9e2954c148ebee4a5e98811afd855c3240f761e5
David Anthoff <anthoff@berkeley.edu>
2017-11-06 10:09:44 -0800

    Add initial code

--- src/FstFiles.jl
+++ src/FstFiles.jl
@@ -1,5 +1,36 @@
 module FstFiles
 
-# package code goes here
+using fstformat, TableTraits, IteratorInterfaceExtensions
+import FileIO
+import IterableTables
+
+struct FstFile
+    filename::String
+    keywords
+end
+
+function load(f::FileIO.File{FileIO.format"Fst"}; keywords...)
+    return FstFile(f.filename, keywords)
+end
+
+TableTraits.isiterable(x::FstFile) = true
+TableTraits.isiterabletable(x::FstFile) = true
+
+function IteratorInterfaceExtensions.getiterator(file::FstFile)
+    df = fstformat.read(file.filename; file.keywords...)
+
+    it = getiterator(df)
+
+    return it
+end
+
+function save(f::FileIO.File{FileIO.format"Fst"}, data)
+    isiterabletable(data) || error("Can't write this data to a fst file.")
+
+    it = getiterator(data)
+
+    fstformat.write(it, f.filename)
+end
+
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,27 @@
 using FstFiles
+using FileIO
+using DataFrames
+using IterableTables
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+@testset "FstFiles" begin
+
+df = DataFrame(a=[1,2,3], b=[1.,2.,3.], c=["A", "B", "C"])
+
+filename = joinpath(@__DIR__, "test.fst")
+
+save(filename, df)
+
+try
+
+    fstfile = load(filename)
+
+    df2 = DataFrame(fstfile)
+
+    @test df == df2
+
+finally
+    rm(filename)
+end
+
+end
